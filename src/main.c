d7867d06c6a13923c3abde45e31497b89849b982
amethystdice <amethystcode@proton.me>
2025-07-27 03:21:55 +0200

    Added amd support and refactored the code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,55 +9,132 @@
 #include "../lib/error-handler.h"
 
 #define BUFFER_SIZE 10
-#define BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/brightness"
-#define MAX_BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/max_brightness"
+#define INTEL_BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/brightness"
+#define MAX_INTEL_BRIGHTNESS_FILE_PATH "/sys/class/backlight/intel_backlight/max_brightness"
+#define AMD_BRIGHTNESS_FILE_PATH "/sys/class/backlight/amdgpu_bl1/brightness"
+#define MAX_AMD_BRIGHTNESS_FILE_PATH "/sys/class/backlight/amdgpu_bl1/max_brightness"
+
+#define INTEL 0
+#define AMD 1
+
+
 #define GITHUB_LINK "\033]8;;https://github.com/amethystdice/intel-backlight-control\033\\\033[34mgithub\033[0m\033]8;;\033\\"
 
 
-int main(int argc,char** argv){
+int displaySelector(){
+    int display_controller = -1;
+    if(access(INTEL_BRIGHTNESS_FILE_PATH,R_OK)==0)
+        display_controller = INTEL;
+    else
+        if(access(AMD_BRIGHTNESS_FILE_PATH,R_OK)==0)
+            display_controller = AMD;
 
-    // check if the user has run the program as "sudo"
-    if(getuid()!=0)
-        exit_with_err_msg("brightness-control: This tool needs admin privileges\nmake sure to run \"sudo brightness-control\"");
-    
-    // check if the program supports the users computer
-
-    if(access(BRIGHTNESS_FILE_PATH,R_OK)!=0){
+    if(display_controller==-1){
         fprintf(stderr, 
             "It looks like this program doesn't support your laptop!\n"
             "Make sure that your devices uses `intel backlight'.\n"
             "If it does and this message still appears make sure to report this bug on %s\n\n"
             "path to the intel-backlight directory: %s\n"
-            ,GITHUB_LINK,BRIGHTNESS_FILE_PATH);
+            ,GITHUB_LINK,INTEL_BRIGHTNESS_FILE_PATH);
         exit(EXIT_FAILURE);
     }
+    return display_controller;
+}
 
-    // check arguments
+char argumentFormatChecker(int argc,char** argv){
 
+    // check for unsupported syntax
     if(argc>2){
         if(fprintf(stderr,"brightness-control: Number of arguments unsupported\nTry 'i-brightness-control --help' for more information\n")<0)
             exit_with_sys_err("fprintf()");
         exit(EXIT_FAILURE);
     }
-    
-    if(argc==2)
-        if((!strncmp(argv[1], "-h", 2)) || (!strncmp(argv[1], "--help", 6))){
-            if(printf(
-            "brightness-control: Wrong number of arguments\n"
-            "Usage: brightness-control\n"
-            "or   : brightness-control [BRIGHTNESS VALUE]\n"
-            "or   : brightness-control max/min\n"    
-            )<0)
-                exit_with_sys_err("printf()");
+
+    if((!strncmp(argv[1], "-h", 2)) || (!strncmp(argv[1], "--help", 6))){
+        if(printf(
+        "Usage: brightness-control\n"
+        "or   : brightness-control [BRIGHTNESS VALUE]\n"
+        "or   : brightness-control max/min\n"    
+        )<0)
+            exit_with_sys_err("printf()");
+        exit(EXIT_SUCCESS);
+    }
+
+    switch(argv[1][0]){
+        case '%':
+            return '%';
+        break;
+
+        case 'm':
+            return 'm';
+        break;
+
+        case 'M':
+            return 'm';
+        break;
+
+        default:
+            return 'v';
+    }
+
+    if(argv[1][0]=='%')
+        return '%';
+    else
+        return 'v';
+
+}
+
+long brightnessSelector(char** argv,char brightness_format,FILE* max_brightness_file){
+
+
+    long current_brightness;
+    long max_brightness;
+
+    if(fscanf(max_brightness_file, "%ld",&max_brightness)==EOF)
+        exit_with_sys_err("fscanf()");
+
+    if(brightness_format=='m'){ //min max
+        if(strlen(argv[1])<3) //security check
+            exit_with_err_msg("unsupported argument detected\n");
+
+        if((!strncmp(argv[1], "max", 3)) || (!strncmp(argv[1], "MAX", 3))){
+            return current_brightness=max_brightness;
         }
 
-    //open brightness and max brightness files
-    
-    char* brightness_file_path = BRIGHTNESS_FILE_PATH; 
-    char* max_brightness_file_path = MAX_BRIGHTNESS_FILE_PATH;
+        if ((!strncmp(argv[1], "min", 3)) || (!strncmp(argv[1], "MIN", 3))){
+            return current_brightness=0;
+        }
+    }
 
-    FILE* brightness_file = fopen(brightness_file_path, "r+");
-    FILE* max_brightness_file = fopen(max_brightness_file_path,"r"); 
+    if(brightness_format=='v'){ //value
+        current_brightness=atol(argv[1]);
+            if((current_brightness<0) || (current_brightness>max_brightness)){
+                if(fprintf(stderr, "Brightness out of the permitted range (0-%ld)\n",max_brightness)<0)
+                    exit_with_sys_err("fprintf()");
+            }
+        return current_brightness;
+    }
+
+    if(brightness_format=='%'){
+        long percent=atol(argv[1]+1);
+        return current_brightness = (percent * max_brightness) / 100;
+    }
+    exit_with_err_msg("no format has been chosen, something has gone wrong\n");
+    return 0; //just to make the compiler shut up
+}
+
+void openFiles(FILE** brightness_file,FILE** max_brightness_file,int display_controller,char* brightness_file_path,char* max_brightness_file_path){
+
+    if(display_controller==INTEL){
+        strncpy(brightness_file_path,INTEL_BRIGHTNESS_FILE_PATH,PATH_MAX); 
+        strncpy(max_brightness_file_path,MAX_INTEL_BRIGHTNESS_FILE_PATH,PATH_MAX);
+    }else{
+        strncpy(brightness_file_path,AMD_BRIGHTNESS_FILE_PATH,PATH_MAX); 
+        strncpy(max_brightness_file_path,MAX_AMD_BRIGHTNESS_FILE_PATH,PATH_MAX);
+    }
+
+    *brightness_file = fopen(brightness_file_path, "r+");
+    *max_brightness_file = fopen(max_brightness_file_path,"r"); 
 
     if( (brightness_file==NULL) || (max_brightness_file==NULL) ){
         fprintf(
@@ -67,55 +145,71 @@ int main(int argc,char** argv){
             ,brightness_file_path,max_brightness_file_path);
         exit(EXIT_SUCCESS);
     }
+}
 
-    //load previous values and max possible brightness
+void noArgumentsCondition(FILE* brightness_file,FILE* max_brightness_file){
+    long previous_brightness_value,max_brightness;
+    fscanf(brightness_file,"%ld",&previous_brightness_value);
+    fscanf(max_brightness_file, "%ld",&max_brightness);
 
-    unsigned int previous_brightness_value;
-    unsigned int max_brightness;
+    if(printf("Current screen brightness: %ld\nBrightness range ( 0 -> %ld )\n",previous_brightness_value,max_brightness)<0)
+        exit_with_sys_err("printf()");
+    exit(EXIT_SUCCESS);
+}
 
-    if(fscanf(brightness_file, "%u",&previous_brightness_value)==EOF)
-        exit_with_sys_err("fscanf()");
-    if(fscanf(max_brightness_file, "%u",&max_brightness)==EOF)
-        exit_with_sys_err("fscanf()");
+void printInFile(FILE* brightness_file,char* brightness_file_path,long current_brightness){
+    if((truncate(brightness_file_path,0))==-1)
+        exit_with_sys_err("truncate()");
+
+    if(fprintf(brightness_file, "%ld",current_brightness)<0)
+        exit_with_sys_err("fprintf()");
     
-    long current_brightness;
-    bool skip_print=false;
+    if(printf("Operation successful! Screen brightness set to %ld\n",current_brightness)<0)
+        exit_with_sys_err("printf()");
 
-    if(argc==1){ // value from command line
-        if(printf("Current screen brightness: %u\nBrightness range ( 0 -> %u )\n",previous_brightness_value,max_brightness)<0)
-            exit_with_sys_err("printf()");
-    }
-    else{ // value from argument
-        if((!strncmp(argv[1], "max", 3)) || (!strncmp(argv[1], "MAX", 3))){
-            current_brightness=max_brightness;
-        }
-        else
-        if ((!strncmp(argv[1], "min", 3)) || (!strncmp(argv[1], "MIN", 3)))
-            current_brightness=0;
-        else{
-            current_brightness=atol(argv[1]);
-            if((current_brightness<0) || (current_brightness>max_brightness)){
-                if(fprintf(stderr, "Brightness out of the permitted range (0-%u)\n",max_brightness)<0)
-                    exit_with_sys_err("fprintf()");
-                skip_print=true;
-            }
-        }
+}
 
-        if(skip_print==false){
-            if((truncate(brightness_file_path,0))==-1)
-                exit_with_sys_err("truncate()");
-        
-            if(fprintf(brightness_file, "%ld",current_brightness)<0)
-                exit_with_sys_err("fprintf()");
-            
-            if(printf("Operation successful! Screen brightness set to %ld\n",current_brightness)<0)
-                exit_with_sys_err("printf()");
-        }
-    }
-    
+void closeFiles(FILE* brightness_file,FILE* max_brightness_file){
     //close files
     if(fclose(brightness_file)==EOF)
         exit_with_sys_err("fclose()");
     if(fclose(max_brightness_file)==EOF)
         exit_with_sys_err("fclose()");
+}
+
+int main(int argc,char** argv){
+
+    // check if the user has run the program as "sudo"
+    if(getuid()!=0)
+        exit_with_err_msg("brightness-control: This tool needs admin privileges\nmake sure to run \"sudo brightness-control\"");
+
+    //check if value is in % or value
+
+
+    // select what type of display is in use
+    int display_controller = displaySelector();
+
+ 
+    FILE* brightness_file = NULL; 
+    FILE* max_brightness_file = NULL;
+    char brightness_file_path[PATH_MAX];
+    char max_brightness_file_path[PATH_MAX];
+
+    // open the appropriate sys files 
+    openFiles(&brightness_file,&max_brightness_file,display_controller,brightness_file_path,max_brightness_file_path);
+    
+    // if the program has only one argument we just need to read the file , not write
+    if(argc==1){
+        noArgumentsCondition(brightness_file,max_brightness_file);
+    }else{
+        // load the new brighness
+        char brightness_format = argumentFormatChecker(argc, argv);
+        long current_brightness = brightnessSelector(argv,brightness_format,max_brightness_file);
+
+        // set new brightness
+        printInFile(brightness_file,brightness_file_path,current_brightness);
+    }
+
+    closeFiles(brightness_file,max_brightness_file);
+
 }
